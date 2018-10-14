b7ae4736012714f6f3b234d6fee47d7c7f9c45e8
FFMaster <FFMaster@HAL-9000>
2018-10-15 10:14:08 +1100

    help!

--- Towers/src/towers/Towers.java
+++ Towers/src/towers/Towers.java
@@ -1,10 +1,10 @@
 package towers;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+//Disks to move around rods, keeps track of where each disk is, 1 is smallest disk
 class Disk {
     private int diskNum;
     private Rod rod;
@@ -23,27 +23,34 @@ class Disk {
     }
     
     public void move(Rod newRod) {
+        //check if this disk is on top of the rod
         if (rod.get(rod.size()-1) != this) {
             System.err.println("this disk is not on the top of the rod");
         }
-        else if (newRod.isEmpty()) {
-            rod.remove(this);
-            newRod.add(this);
-            rod = newRod;
-            System.out.println("Disk " + diskNum + " moves to Rod " + newRod.getRodNum());
-        }
-        else if ((this.diskNum > newRod.get(newRod.size()-1).diskNum) ) {
-            System.err.println("target rod has a smaller disk on top");
-        }
         else {
-            rod.remove(this);
-            newRod.add(this);
-            rod = newRod;
-            System.out.println("Disk " + diskNum + " moves to Rod " + newRod.getRodNum());
+            //check if target rod is empty, move disk to that rod if true
+            if (newRod.isEmpty()) {
+                rod.remove(this);
+                newRod.add(this);
+                rod = newRod;
+                System.out.println("Disk " + diskNum + " moves to Rod " + newRod.getRodNum());
+            }
+            //rod is not empty, check if this disk is larget than the top disk of target rod, no move, give error message
+            else if ((this.diskNum > newRod.get(newRod.size()-1).diskNum) ) {
+                System.err.println("target rod has a smaller disk on top");
+            }
+            //rod is not empty and this disk is smaller than the top disk of target rod, move
+            else {
+                rod.remove(this);
+                newRod.add(this);
+                rod = newRod;
+                System.out.println("Disk " + diskNum + " moves to Rod " + newRod.getRodNum());
+            }
         }
     }
 }
 
+//mainly for keeping track of which rod is which
 class Rod extends ArrayList<Disk> {
     
     private int rodNum;
@@ -60,17 +67,23 @@ class Rod extends ArrayList<Disk> {
 
 public class Towers {
     
-    private static boolean hanoiAlgorithm(ArrayList<Disk> disks, Rod[] rods) {
+    //recursion statement
+    private static void hanoiAlgorithm(ArrayList<Disk> disks, Rod[] rods) {
+        //trivial case, no more disks to move
         if (disks.isEmpty()) {
-            return true;
         }
         else {
+            //create an array of disks with the largest one missing(n - 1)
             ArrayList<Disk> disksRecursion = new ArrayList<>();
             disksRecursion.addAll(disks);
             disksRecursion.remove(0);
+            
             //System.out.println("disks total after removing: " + disksRecursion.size());
+            
+            //move all disks except largest out of the way
             hanoiAlgorithm(disksRecursion, rods);
             
+            //get the largest disk and move it
             Disk d = disks.get(0);
             if ((d.getDiskNum() % 2) == 1) {
                 d.move(rods[((d.getRodNum() + 1) % 3)]);
@@ -78,8 +91,9 @@ public class Towers {
             else {
                 d.move(rods[((d.getRodNum() + 2) % 3)]);
             }
+            
+            //move the disks on top of the largest disk
             hanoiAlgorithm(disksRecursion, rods);
-            return false;
         }
     }
 
